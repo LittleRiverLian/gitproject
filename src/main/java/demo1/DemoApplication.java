31c837cfb11f801200858406eb230a6cbd37f5a8
lxj <772801088@qq.com>
2018-10-02 09:49:16 +0800

    test24444444444 44455

---
+++ src/main/java/demo1/DemoApplication.java
@@ -0,0 +1,13 @@
+package demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        //test test test test test5  test6 test7 test8
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
